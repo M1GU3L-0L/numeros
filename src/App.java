0282c34ba69776f37923b97a58f01761b9c41c21
M1GU3L-0L <edwarol2004@gmail.com>
2025-09-30 11:57:38 -0500

    second commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        var numero = 1;
+        var numero2 = 2;
+        var resultado = numero + numero2;
+        System.out.println(resultado);
+
     }
 }
